c9ff648123cbe972206f0207affb57e304dfd7a9
VeygaX <84923630+veygax@users.noreply.github.com>
2024-09-15 18:26:08 +0100

    Create createshortcuts.bat

---
+++ src/Playbook/Executables/createshortcuts.bat
@@ -0,0 +1,24 @@
+@echo off
+setlocal
+
+:: Define variables
+set "steamPath=C:\Program Files (x86)\Steam\Steam.exe"
+set "desktop=%USERPROFILE%\Desktop"
+set "shortcutPath=%desktop%\Return to Gaming Mode.lnk"
+set "iconUrl=https://drive.google.com/uc?export=download&id=1KdbDLKe36HtutAfzo6zzUcNEKZ1_WMYq"
+set "iconFolder=%USERPROFILE%\DeckXP"
+set "iconPath=%iconFolder%\steam_bigpicture.ico"
+
+mkdir "%iconFolder%"
+
+powershell -Command "Invoke-WebRequest -Uri '%iconUrl%' -OutFile '%iconPath%'"
+
+powershell -Command ^
+  $wShell = New-Object -ComObject WScript.Shell; ^
+  $shortcut = $wShell.CreateShortcut('%shortcutPath%'); ^
+  $shortcut.TargetPath = '%steamPath%'; ^
+  $shortcut.Arguments = '-start steam://open/bigpicture'; ^
+  $shortcut.IconLocation = '%iconPath%'; ^
+  $shortcut.Save()
+
+endlocal
