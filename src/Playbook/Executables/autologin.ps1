4c44324363f134240bb66f6daf404462a78bbb2a
VeygaX <84923630+veygax@users.noreply.github.com>
2024-09-20 10:29:07 -0700

    fixed handheldcompanion not being silent, and autologin

--- src/Playbook/Executables/autologin.ps1
+++ src/Playbook/Executables/autologin.ps1
@@ -3,14 +3,14 @@ $username = (Get-WmiObject -Class Win32_ComputerSystem).UserName
 $Password = "deckxp"
 Set-LocalUser -Name $username -Password (ConvertTo-SecureString -AsPlainText $Password -Force)
 
-$RegPath = "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon"
+$RegPath = "Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon"
 
-Set-ItemProperty -Path $RegPath -Name "AutoAdminLogon" -Value "1" -Type STRING
-Set-ItemProperty -Path $RegPath -Name "DefaultUserName" -Value $username -Type STRING
-Set-ItemProperty -Path $RegPath -Name "DefaultPassword" -Value $Password -Type STRING
-Set-ItemProperty -Path $RegPath -Name "AutoLogonCount" -Value 1 -Type DWORD
+Set-ItemProperty -Path $RegPath -Name "AutoAdminLogon" -Value "1"
+Set-ItemProperty -Path $RegPath -Name "DefaultUserName" -Value $username
+Set-ItemProperty -Path $RegPath -Name "DefaultPassword" -Value $Password
+Set-ItemProperty -Path $RegPath -Name "AutoLogonCount" -Value 1
 
-Set-ItemProperty -Path "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" -Name "DisableCAD" -Value "1"
+Set-ItemProperty -Path "Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" -Name "DisableCAD" -Value "1"
 
-$PasswordLessPath = "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Authentication\LogonUI"
+$PasswordLessPath = "Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Authentication\LogonUI"
 Set-ItemProperty -Path $PasswordLessPath -Name "DevicePasswordLessBuildVersion" -Value "0"
