5eaec17ba4b4abb95eff274ea2c20ea608bb4686
VeygaX <84923630+veygax@users.noreply.github.com>
2024-09-20 07:58:25 -0700

    fixed display scaling

--- src/Playbook/Executables/setdisplayscale.ps1
+++ src/Playbook/Executables/setdisplayscale.ps1
@@ -1,42 +1,21 @@
-# Load the default user hive and set properties
-REG LOAD HKU\Default_User C:\users\default\ntuser.dat
-New-ItemProperty -Path registry::"HKU\Default_User\Control Panel\Desktop" -Name LogPixels -Value 96 -Type DWord
-New-ItemProperty -Path registry::"HKU\Default_User\Control Panel\Desktop" -Name Win8DpiScaling -Value 1 -Type DWord
-REG UNLOAD HKU\Default_User
-
-# Define the SID pattern
-$PatternSID = 'S-1-5-21-\d+-\d+-\d+-\d+$'
-
-# Get the list of user profiles
-$ProfileList = Get-ItemProperty 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\ProfileList\*' | 
-    Where-Object { $_.PSChildName -match $PatternSID } | 
-    Select-Object @{Name="SID"; Expression={$_.PSChildName}}, 
-                   @{Name="UserHive"; Expression={"$($_.ProfileImagePath)\ntuser.dat"}}, 
-                   @{Name="Username"; Expression={$_.ProfileImagePath -replace '^(.*[\\\/])', ''}}
-
-# Get the list of currently loaded user hives
-$LoadedHives = Get-ChildItem Registry::HKEY_USERS | 
-    Where-Object { $_.PSChildName -match $PatternSID } | 
-    Select-Object @{Name="SID"; Expression={$_.PSChildName}}
-
-# Determine which profiles are not loaded
-$UnloadedHives = Compare-Object $ProfileList.SID $LoadedHives.SID | 
-    Select-Object @{Name="SID"; Expression={$_.InputObject}}, 
-                   UserHive, 
-                   Username
-
-# Process each profile
-foreach ($item in $ProfileList) {
-    if ($item.SID -in $UnloadedHives.SID) {
-        reg load HKU\$($item.SID) $($item.UserHive) | Out-Null
-    }
-
-    "{0}" -f $($item.Username) | Write-Output
-    New-ItemProperty -Path registry::"HKU\$($item.SID)\Control Panel\Desktop" -Name LogPixels -Value 96 -Type DWord -Force
-    New-ItemProperty -Path registry::"HKU\$($item.SID)\Control Panel\Desktop" -Name Win8DpiScaling -Value 1 -Type DWord -Force
-
-    if ($item.SID -in $UnloadedHives.SID) {
-        [gc]::Collect()
-        reg unload HKU\$($item.SID) | Out-Null
-    }
+function Set-Scaling {
+    # Posted by IanXue-MSFT on
+    # https://learn.microsoft.com/en-us/answers/questions/197944/batch-file-or-tool-like-powertoy-to-change-the-res.html
+    # $scaling = 0 : 100% (default)
+    # $scaling = 1 : 125% 
+    # $scaling = 2 : 150% 
+    # $scaling = 3 : 175% 
+    param($scaling)
+    $source = @'
+    [DllImport("user32.dll", EntryPoint = "SystemParametersInfo")]
+    public static extern bool SystemParametersInfo(
+                      uint uiAction,
+                      uint uiParam,
+                      uint pvParam,
+                      uint fWinIni);
+'@
+    $apicall = Add-Type -MemberDefinition $source -Name WinAPICall -Namespace SystemParamInfo -PassThru
+    $apicall::SystemParametersInfo(0x009F, $scaling, $null, 1) | Out-Null
 }
+
+Set-Scaling -scaling 0
