2be97c3594de2bcbbc74f1340525688a6d4a3cc0
VeygaX <84923630+veygax@users.noreply.github.com>
2024-09-22 02:15:13 +0100

    Update setup-postinstall.bat

--- src/Playbook/Executables/Scripts/setup-postinstall.bat
+++ src/Playbook/Executables/Scripts/setup-postinstall.bat
@@ -3,15 +3,26 @@ setlocal
 
 set "currentDir=%~dp0"
 set "startupFolder=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup"
-set "scriptName=Scripts\postinstall.ps1"
+set "scriptName=postinstall.ps1"
+set "setScaleScript=setdisplayscale.ps1"
+set "setLandscapeScript=setdisplaytolandscape.ps1"
+set "targetFolder=%userprofile%\DeckXP\Scripts"
 set "shortcutPath=%startupFolder%\DeckXP-Postinstall.lnk"
+
+if not exist "%targetFolder%" (
+    mkdir "%targetFolder%"
+)
+
+copy "%currentDir%Scripts\%scriptName%" "%targetFolder%" /Y
+copy "%currentDir%Scripts\%setScaleScript%" "%targetFolder%" /Y
+copy "%currentDir%Scripts\%setLandscapeScript%" "%targetFolder%" /Y
 
 powershell -Command ^
   $wShell = New-Object -ComObject WScript.Shell; ^
   $shortcut = $wShell.CreateShortcut('%shortcutPath%'); ^
   $shortcut.TargetPath = 'powershell.exe'; ^
-  $shortcut.Arguments = '-ExecutionPolicy Bypass -File "%currentDir%%scriptName%"'; ^
-  $shortcut.WorkingDirectory = '%currentDir%'; ^
+  $shortcut.Arguments = '-ExecutionPolicy Bypass -File "%targetFolder%\%scriptName%"'; ^
+  $shortcut.WorkingDirectory = '%targetFolder%'; ^
   $shortcut.Save()
 
 endlocal
